22639d7bb36e4e86b9f99a068f49fb8f6b19cc3d
Khushal Yadav <khushaly04@gmail.com>
2026-06-12 12:41:56 +0530

    stupid mistakes

---
+++ Frontend/src/lib/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const API = `${process.env.REACT_APP_BACKEND_URL}/api`;
+
+export const api = axios.create({
+  baseURL: API,
+  withCredentials: true,
+});
+
+// Attach Bearer token (also used by admin endpoints)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("admin_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export const fileUrl = (path) => {
+  if (!path) return "";
+  if (path.startsWith("http")) return path;
+  return `${API}/files/${path}`;
+};
+
+export const formatINR = (n) =>
+  new Intl.NumberFormat("en-IN", { style: "currency", currency: "INR", maximumFractionDigits: 0 }).format(n || 0);
